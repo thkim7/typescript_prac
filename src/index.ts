768ab329300029cf98d7f8e94f4f69fd116abac9
thkim7 <xogusrla7@gmail.com>
2025-11-29 15:21:59 +0900

    init commit

---
+++ src/index.ts
@@ -0,0 +1,18 @@
+class User {
+    name: string = "";
+    age: number = 0;
+
+    isAdult(): boolean {
+        return this.age >= 20;
+    }
+
+    setAge(newAge: number) {
+        this.age = newAge;
+    }
+}
+
+const uhyo = new User();
+console.log(uhyo.isAdult());
+
+uhyo.setAge(26);
+console.log(uhyo.isAdult());
